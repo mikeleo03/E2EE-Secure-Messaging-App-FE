2b0900712f24cdd957a61e5e5d3f5bee6c67826e
haikallf <haikallzrd@gmail.com>
2022-07-21 01:16:49 +0700

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReduxDemo from './pages/ReduxDemo';
 import './main.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <div>
       Hello, World
       <ReduxDemo />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/redux-demo" element={<ReduxDemo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
