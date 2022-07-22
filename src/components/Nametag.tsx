bbd8d708aa04fc40e50569d316d248a94fdc29b3
haikallf <haikallzrd@gmail.com>
2022-07-23 01:27:44 +0700

    add macos-like scrollbar

---
+++ src/components/Nametag.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface NameTagProps {
+  nama: string;
+  nim: string;
+}
+
+const Nametag: React.FC<NameTagProps> = ({ nama, nim }) => {
+  return (
+    // eslint-disable-next-line max-len
+    <div className="flex w-[310px] h-[100px] bg-secondaryBlue justify-between items-center rounded-2xl pr-8 pl-5 py-3.5">
+      <div className="min-w-[71px] inline-block h-[71px] w-[71px] rounded-full bg-black"></div>
+      <div className="w-full">
+        <div className="text-white flex flex-col justify-center items-center">
+          {nama}
+        </div>
+        <div className="text-white flex flex-col justify-center items-center">
+          {nim}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nametag;
--- src/pages/Chatroom.tsx
+++ src/pages/Chatroom.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Heading from '../components/Heading';
+import Nametag from '../components/Nametag';
 
 const Chatroom: React.FC = () => {
-  return <Heading className="text-center">Chat Room</Heading>;
+  return (
+    <>
+      <Heading className="text-center">Chat Room</Heading>
+      <Nametag nama="Haikal Lazuardi Fadil" nim="13519027" />
+    </>
+  );
 };
 
 export default Chatroom;
