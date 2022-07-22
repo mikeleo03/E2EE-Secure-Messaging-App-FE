4635909206a9cd49cf00c067cbcbbd5b968a2eb7
haikallf <haikallzrd@gmail.com>
2022-07-23 06:32:17 +0700

    renders chat bubble

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,11 +1,29 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
+import { ChatData } from '../interfaces/chat';
+import ChatBubble from './ChatBubble';
 
-const ChatContainer: React.FC = () => {
+interface ChatContainerProps {
+  myName?: string;
+  myNIM?: string;
+  chatData: ChatData[];
+}
+
+const ChatContainer: React.FC<ChatContainerProps> = ({
+  myName,
+  myNIM,
+  chatData,
+}) => {
   return (
     <div className="bg-white h-[650px] w-[777px] rounded-[15px]">
-      <div className="h-[95%] w-[100%] rounded-t-[15px]"></div>
+      <div className="h-[95%] w-[100%] rounded-t-[15px] flex flex-col max-w-[100%] px-2 pt-2 ">
+        {chatData.map((chat, idx) => (
+          <ChatBubble key={idx} sent={myNIM === chat.sender ? true : false}>
+            {chat.message}
+          </ChatBubble>
+        ))}
+      </div>
       <div className="h-[113px] bg-white border-solid border-t-2 border-primaryOrange flex justify-between items-center px-8  rounded-b-[15px]">
         <textarea
           placeholder="Enter a message"
