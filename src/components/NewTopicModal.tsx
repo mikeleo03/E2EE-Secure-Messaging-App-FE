2de4ef4c24c34d9f111166e709c8ed96466f8d24
fikrikhoironn <fikrikf470@gmail.com>
2022-07-26 21:59:02 +0700

    feat: tutorial modal, new topic modal, and report modal

---
+++ src/components/NewTopicModal.tsx
@@ -0,0 +1,71 @@
+import { Button, Form, Input, Modal, Radio, RadioChangeEvent } from 'antd';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setNewTopicModal } from '../redux/actions/modal';
+import { modalSelector } from '../redux/selectors/modal';
+import Heading from './Heading';
+
+const NewTopicModal: React.FC = () => {
+  const { new_topic_modal } = useSelector(modalSelector);
+  const dispatch = useDispatch();
+
+  const [form] = Form.useForm();
+
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [otherValue, setOtherValue] = useState('');
+  const [modalText, setModalText] = useState('Content of the modal');
+
+  const handleOtherChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setOtherValue(e.target.value);
+  };
+
+  const handleOk = () => {
+    setModalText('Great! Your topic has been added to wait list!');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      dispatch(setNewTopicModal(false));
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    dispatch(setNewTopicModal(false));
+  };
+  return (
+    <>
+      <Modal
+        okButtonProps={{
+          disabled: otherValue === '',
+        }}
+        visible={true}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+        className="modalStyle rounded-[15px]"
+        width={486}
+      >
+        <Heading className="text-center mt-[80px]">New Topic</Heading>
+        <Form.Item
+          name="topic"
+          rules={[
+            {
+              required: otherValue === '',
+              message: 'Please write new topic!',
+            },
+          ]}
+        >
+          <Input.TextArea
+            placeholder="Write new topic"
+            value={otherValue}
+            onChange={handleOtherChange}
+            maxLength={26}
+            showCount={true}
+            autoSize={{ minRows: 1, maxRows: 2 }}
+          />
+        </Form.Item>
+      </Modal>
+    </>
+  );
+};
+
+export default NewTopicModal;
---
+++ src/components/TutorialModal.tsx
@@ -0,0 +1,71 @@
+import { Button, Form, Input, Modal, Radio, RadioChangeEvent } from 'antd';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTutorialModal } from '../redux/actions/modal';
+import { modalSelector } from '../redux/selectors/modal';
+import Heading from './Heading';
+import Card from './Card';
+import CaptionCard from './CaptionCard';
+
+const TutorialModal: React.FC = () => {
+  const { tutorial_modal } = useSelector(modalSelector);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Modal
+        visible={true}
+        className="modalStyle rounded-[15px] bg-primaryBlue"
+        footer={null}
+        width={930}
+        bodyStyle={{
+          backgroundColor: '#79C7D4',
+        }}
+      >
+        <Heading className="text-center">Tutorial</Heading>
+        <div className="">
+          <div className="flex flex-col items-center justify-center gap-5">
+            <div>
+              <CaptionCard
+                text="1. Choose Your Topic."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 `}
+              />
+            </div>
+            <div>
+              <CaptionCard
+                text="2. Click Finding Match Button to find a match."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 h-[80px]`}
+              />
+            </div>
+            <div>
+              <CaptionCard
+                text="3. Wait for a match to occur."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 h-[46px]`}
+              />
+            </div>
+            <div>
+              <CaptionCard
+                text="4. Start Chatting with your match."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 h-[46px]`}
+              />
+            </div>
+            <div>
+              <CaptionCard
+                text="5. Click Reveal My Name button together to reveal name."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 h-[80px]`}
+              />
+            </div>
+            <div>
+              <CaptionCard
+                text="6. Click Next button for finding new match."
+                className={`font-alegreyasans text-subHeading flex items-center pl-10 h-[80px]`}
+              />
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default TutorialModal;
