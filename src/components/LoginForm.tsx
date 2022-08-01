2ea385d5bd2af25bcc72f38676cc3c6c0d9288dc
raniadf <16520006@mahasiswa.itb.ac.id>
2022-08-01 12:49:29 +0700

    fix responsive

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,6 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input, Modal} from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Modal, Radio } from 'antd';
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import PrivacyPolicy from './PrivacyPolicy';
 
 const LoginForm: React.FC = () => {
@@ -12,19 +10,6 @@ const LoginForm: React.FC = () => {
   };
 
   const [visible, setVisible] = useState(false);
-
-  const navigate = useNavigate();
-  const navigateToHome = () => {
-    if (value != ''){
-      navigate('/');
-    }
-  };
-
-  const [value, setValue] = useState('');
-
-  const onChange = (e: any) => {
-    setValue(e.target.value);
-  };
 
   return (
     <Form
@@ -54,7 +39,7 @@ const LoginForm: React.FC = () => {
       <Form.Item>
         <div className='flex justify-center h-auto'>
           <Button type="primary" htmlType='submit'
-          style={{background:'#4699B7', borderRadius:'15px', borderColor:"#4699B7"}}>
+            style={{background:'#4699B7', borderRadius:'15px', borderColor:'#4699B7'}}>
             <p className='font-alegreya text-body px-5 text-white'>Sign In</p>
           </Button>
         </div>
@@ -67,37 +52,7 @@ const LoginForm: React.FC = () => {
           width={1000}
           closable={false}
         >
-          <div className='flex justify-center h-[50vh]'>
-            <div className='bg-[#FFFFFF] w-[90vw] h-[50vh] top-10 rounded-lg'>
-              <div className='flex justify-center h-auto'>
-                <h1 className='font-magilio text-subHeading pt-2'>Terms and Conditions</h1>
-              </div>
-              <div className='overflow-y-auto h-[37vh] px-5'>
-               <PrivacyPolicy/>
-              </div>
-            </div>
-          </div>
-          <div className='relative h-auto z-40'>
-            <Form>
-              <Form.Item
-                name="radio"
-                rules={[{ required: true, message: "You must agree to the Terms and Conditions to use the app." }]}
-                className='p-0 font-alegreyasans'
-              >
-                <Radio.Group onChange={onChange} value={value} className='h-5'>
-                  <Radio value={"checked"}>
-                    <p className='font-alegreyasans text-body text-white font-bold pt-3'>I have read and agree to these terms and conditions</p>
-                  </Radio>
-                </Radio.Group>
-              </Form.Item>
-              <div className='flex justify-center h-auto'>
-                <Button htmlType='submit' onClick={navigateToHome} 
-                style={{background:'#FFA06E', borderRadius:'15px', borderColor:"#FFA06E"}}>
-                  <p className='font-alegreya text-body px-16 text-white'>Submit</p>
-                </Button>
-              </div>
-            </Form>
-          </div>
+          <PrivacyPolicy/>
         </Modal>
       </Form.Item>
     </Form>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,19 +5,20 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="bg-[#FFC176] w-[100vw] h-[100vh] relative py-8">
-        <div className="absolute w-full h-[10vh] top-36 left-0 right-0 z-2">
-            <div className="flex justify-center">
-                <div className="bg-[#F27267] w-[25vw] h-[15vh] w-[30vw] top-0 z-40 rounded-lg"/>
-            </div>
+      <div className="absolute w-full h-[10vh] top-32 lg:top-52 left-0 right-0 z-2">
+        <div className="flex justify-center">
+          <div className="bg-[#F27267] xs:w-[54vw] sm:w-[40vw] lg:w-[25vw] xs:h-40 lg:h-24 z-40 rounded-lg"/>
         </div>
-        <div className="absolute w-full h-[50vh] bottom-0 z-2">
-          <div className="flex justify-center">
-            <div className='bg-[#F7F7F8] w-[35vw] rounded-lg px-16 pt-9 pb-2'>
-              <LoginForm/>
-            </div>  
-          </div>
+      </div>
+      <div className="absolute w-full xs:h-[50vh] lg:h-80 xs:bottom-0 lg:top-96 z-2">
+        <div className="flex justify-center">
+          <div className='bg-[#F7F7F8] xs:w-[70vw] sm:w-[50vw] lg:w-[35vw] rounded-lg 
+            xs:px-8 lg:px-16 xs:pt-4 lg:pt-9 xs:pb-0 lg:pb-2'>
+            <LoginForm/>
+          </div>  
         </div>
-        <div className="absolute bg-[#79C7D4] w-full h-[30vh] top-0 z-1"/>
+      </div>
+      <div className="absolute bg-[#79C7D4] w-full xs:h-52 lg:h-64 top-0 z-1"/>
     </div>
   );
 };
