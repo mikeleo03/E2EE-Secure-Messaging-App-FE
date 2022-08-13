aae5fe0dcacb4658d7d3486b3a886db258979de0
haikallf <haikallzrd@gmail.com>
2022-08-14 02:19:24 +0700

    fix button font size issue

--- src/components/OrangeButton.tsx
+++ src/components/OrangeButton.tsx
@@ -15,7 +15,7 @@ const OrangeButton: React.FC<OrangeButtonProps> = ({
     <div
       onClick={onClick}
       className={`font-alegreya bg-secondaryOrange xs:w-[90%] lg:w-[281px] xs:h-[25px] lg:h-[47px] xs:my-6 lg:mt-0 
-      xs:text-caption lg:text-subHeading text-white flex justify-center items-center rounded-[15px] 
+      xs:text-body lg:text-subHeading text-white flex justify-center items-center rounded-[15px] 
       cursor-pointer ${className}`}
     >
       {children}
