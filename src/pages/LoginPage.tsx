d60ff6b0d446298fdc4f0d32dba3466017fd1d09
haikallf <haikallzrd@gmail.com>
2022-08-17 17:56:26 +0700

    fix login page z-index

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -11,7 +11,7 @@ const LoginPage: React.FC = () => {
       <div className="h-screen w-screen flex items-center flex-col gap-48 bg-[#FFC176]">
         <div className="w-full h-[200px] lg:h-[225px] flex items-center justify-center relative bg-[#79C7D4]">
           <div
-            className="bg-[#F27267] w-[54vw] sm:w-[40vw] lg:w-[25vw] h-40 lg:h-24 z-40 rounded-lg
+            className="bg-[#F27267] w-[54vw] sm:w-[40vw] lg:w-[25vw] h-40 lg:h-24 rounded-lg
         absolute mx-auto xs:top-28 lg:top-44 flex justify-center items-center"
           >
             <Heading className="text-white m-auto"> amir</Heading>
@@ -25,7 +25,7 @@ const LoginPage: React.FC = () => {
         </div>
         <div
           className="bg-[#F7F7F8] xs:w-[70vw] sm:w-[50vw] lg:w-[35vw] rounded-lg
-        xs:px-8 lg:px-16 xs:pt-4 lg:pt-9 xs:pb-0 lg:pb-2 m-[-40px]"
+        xs:px-8 lg:px-16 xs:pt-4 lg:pt-9 xs:pb-0 lg:pb-2 m-[-40px] relative z-[2]"
         >
           <LoginForm />
         </div>
