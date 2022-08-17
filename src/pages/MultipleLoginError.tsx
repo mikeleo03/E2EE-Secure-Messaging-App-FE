ad1c6a42c7ec01bce023885bbca48f197cc3bf50
haikallf <haikallzrd@gmail.com>
2022-08-17 15:14:07 +0700

    add relogin on connection error adn cleanup

--- src/pages/MultipleLoginError.tsx
+++ src/pages/MultipleLoginError.tsx
@@ -2,25 +2,6 @@ import React from 'react';
 import MultipleLogin from '../components/MultipleLogin';
 
 const MultipleLoginError: React.FC = () => {
-  //   const navigate = useNavigate();
-  //   const [multipleLogin, setMultipleLogin] = useState(0);
-
-  //   useEffect(() => {
-  //     socket.on('connect_error', (err) => {
-  //       setMultipleLogin(1);
-  //     });
-
-  //     setTimeout(() => {
-  //       if (multipleLogin !== 1) {
-  //         setMultipleLogin(-1);
-  //       }
-  //     }, 1000);
-
-  //     if (multipleLogin === -1) {
-  //       navigate('/', { replace: true });
-  //     }
-  //     console.log(multipleLogin);
-  //   }, [multipleLogin]);
   return <MultipleLogin />;
 };
 
