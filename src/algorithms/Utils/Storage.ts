2d056c443ab778b2537ac8e66e09e89c7a8aa309
mikeleo03 <leonmichael463@gmail.com>
2024-05-22 16:24:29 +0700

    [Refactor] unused localStorage functionality

--- src/algorithms/Utils/Storage.ts
+++ src/algorithms/Utils/Storage.ts
@@ -1,9 +1,5 @@
 import * as fs from 'fs';
 import { ECPoint } from '../ECC/EllipticCurve';
-import { LocalStorage } from 'node-localstorage';
-import config from '../../config';
-
-const localStorage = new LocalStorage('');
 
 /**
  * Save single key point to a file
@@ -44,28 +40,4 @@ export function readKeyPairFromFile(filename: string): ECPoint {
     const data = fs.readFileSync(filename, 'utf-8');
     const { x, y } = JSON.parse(data);
     return new ECPoint(BigInt(x as string), BigInt(y as string));
-}
-
-/**
- * Save key pair point to localStorage.
- * @param {string} key - The key under which the point is stored.
- * @param {ECPoint} point - The key pair point to be stored.
- */
-export function saveKeyPairToLocalStorage(key: string, point: ECPoint): void {
-    const data = JSON.stringify({ x: point.x.toString(), y: point.y.toString() });
-    localStorage.setItem(key, data);
-}
-
-/**
- * Retrieve key pair point from localStorage.
- * @param {string} key - The key under which the point is stored.
- * @returns {ECPoint | null} The key pair point stored in localStorage, or null if not found.
- */
-export function getKeyPairFromLocalStorage(key: string): ECPoint | null {
-    const data = localStorage.getItem(key);
-    if (data) {
-        const { x, y } = JSON.parse(data);
-        return new ECPoint(BigInt(x as string), BigInt(y as string));
-    }
-    return null;
 }
