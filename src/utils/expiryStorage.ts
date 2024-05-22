d3c5b58baa68f6752ef205be3a68fa992d1a4526
mikeleo03 <leonmichael463@gmail.com>
2024-05-22 23:55:32 +0700

    [Feat] Expiry storage to handle token expiration

---
+++ src/utils/expiryStorage.ts
@@ -0,0 +1,27 @@
+function setWithExpiry(key: string, value: string, ttl: number) {
+    const now = new Date();
+    localStorage.setItem(key, value);
+    localStorage.setItem(key + '-expiry', (now.getTime() + ttl).toString());
+}
+
+function getWithExpiry(key: string) {
+    const now = new Date();
+    const expiryTimeString = localStorage.getItem(key + '-expiry');
+    
+    if (expiryTimeString === null) {
+        // Value not found
+        return null;
+    }
+
+    const expiryTime = parseInt(expiryTimeString, 10);
+
+    if (isNaN(expiryTime) || now.getTime() > expiryTime) {
+        localStorage.removeItem(key);
+        localStorage.removeItem(key + '-expiry');
+        return null;
+    }
+
+    return localStorage.getItem(key);
+}
+
+export { setWithExpiry, getWithExpiry };
